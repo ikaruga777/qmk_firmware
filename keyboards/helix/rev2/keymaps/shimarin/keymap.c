ba6207667734ff47c62a694911a4e863da4b52fc
ikaruga <ikaruga777@gmail.com>
2018-02-22 00:31:40 +0900

    キーをタップしたときと同時押しでshiftとenterを切り替えできるように

--- keyboards/helix/rev2/keymaps/shimarin/keymap.c
+++ keyboards/helix/rev2/keymaps/shimarin/keymap.c
@@ -84,15 +84,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,  KC_ENT,  KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, KC_RSFT , \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,  KC_ENT,  KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH,  SFT_T(KC_ENT), \
       ADJUST,  KC_ESC,  KC_LGUI, KC_LALT, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
     /* Lower
     * ,-----------------------------------------.             ,-----------------------------------------.
-    * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  | del  |
+    * |   ~  |   !  |   "  |   #  |   $  |   %  |             |   &  |   '  |   (  |   )  |   )  | del  |
     * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-    * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   [  |   ]  |  ^   |
+    * |   ~  |   !  |   "  |   #  |   $  |   %  |             |   &  |   '  |   *  |   [  |   ]  |  ^   |
     * |------+------+------+------+------+------|             |------+------+------+------+------+------|
     * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   _  |   +  |   {  |   }  |  |   |
     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `-------------------------------------------------------------------------------------------------'
     */
     [_RAISE] = KEYMAP( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    M_PSHEET,M_NSHEET,KC_DEL, \
       KC_LCTL, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   JP_MINS, JP_EQL,  KC_PGUP, KC_HOME, JP_BSLS, \
       KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  JP_UNDS, KC_PGUP, KC_PGDN, KC_END,  KC_ENT, \
