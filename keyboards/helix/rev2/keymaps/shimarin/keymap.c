ba1ebe7e7564efa90c8c5bbd5143a949ad52f4c1
ikaruga <ikaruga777@gmail.com>
2018-02-25 01:39:16 +0900

    カッコの位置調整とpgdn系の位置調整

--- keyboards/helix/rev2/keymaps/shimarin/keymap.c
+++ keyboards/helix/rev2/keymaps/shimarin/keymap.c
@@ -84,28 +84,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,  KC_ENT,  KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH,  SFT_T(KC_ENT), \
-      ADJUST,  KC_ESC,  KC_LGUI, KC_LALT, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH,  SFT_T(KC_ENT), \
+      KC_LGUI, ADJUST,  KC_LGUI, KC_LALT, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
     /* Lower
     * ,-----------------------------------------.             ,-----------------------------------------.
-    * |   ~  |   !  |   "  |   #  |   $  |   %  |             |   &  |   '  |   (  |   )  |   )  | del  |
+    * |   ~  |   !  |   "  |   #  |   $  |   %  |             |   &  |   '  |   (  |   )  |  =   | del  |
     * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-    * |   ~  |   !  |   "  |   #  |   $  |   %  |             |   &  |   '  |   *  |   [  |   ]  |  ^   |
+    * |   ~  |   !  |   "  |   #  |   $  |   %  |             |   &  |   '  |   [  |   ]  |      |  ^   |
     * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-    * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   _  |   +  |   {  |   }  |  |   |
+    * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   _  |   {  |   }  |      |  |   |
     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  (   |   )  |  F12 |   -  | pgup | pgdn | Home | End  |
+    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  (   |   )  |  F12 |   -  |      |      |      | ENT  |
     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
     * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
     * `-------------------------------------------------------------------------------------------------'
     */
     [_LOWER] = KEYMAP( \
-      KC_TILD, JP_EXLM, JP_DQT,  JP_HASH, JP_DLR,  JP_PERC,                   JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_RPRN, KC_DEL, \
-      KC_TILD, JP_EXLM, JP_DQT,   JP_HASH, JP_DLR,  JP_PERC,                  JP_AMPR, JP_QUOT, JP_LBRC, JP_RBRC, JP_RBRC, JP_CIRC, \
-      KC_LCTL, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   JP_MINS, JP_LCBR, KC_PGUP, KC_HOME, JP_PIPE, \
-      KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  JP_LPRN, JP_RPRN, KC_F12,  JP_UNDS, KC_PGUP, KC_PGDN, KC_END,  KC_ENT, \
+      KC_TILD, JP_EXLM, JP_DQT,  JP_HASH, JP_DLR,  JP_PERC,                   JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_EQL, KC_DEL, \
+      KC_TILD, JP_EXLM, JP_DQT,   JP_HASH, JP_DLR,  JP_PERC,                  JP_AMPR, JP_PLUS, JP_LBRC, JP_RBRC, _______, JP_CIRC, \
+      KC_LCTL, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   JP_MINS, JP_LCBR, JP_RCBR, _______, JP_PIPE, \
+      KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  JP_LPRN, JP_RPRN, KC_F12,  JP_UNDS, _______, _______, _______,  KC_ENT, \
       ADJUST,  KC_ESC,  KC_LGUI, KC_LALT, EISU,    _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
@@ -113,11 +113,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * ,-----------------------------------------.             ,-----------------------------------------.
     * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Bksp |
     * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-    * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Del  |
+    * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  | PRST | NTST | Del  |
     * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-    * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   -  |   =  |   [  |   ]  |  \   |
+    * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   -  |   =  | pgup | pgdn |      |
     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |   _  | pgup | pgdn | Home | End  |
+    * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |      |  F12 |   _  |   \  | home | end  | ent  |
     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
     * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
     * `-------------------------------------------------------------------------------------------------'
@@ -125,8 +125,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = KEYMAP( \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    M_PSHEET,M_NSHEET,KC_DEL, \
-      KC_LCTL, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   JP_MINS, JP_EQL,  KC_PGUP, KC_HOME, JP_BSLS, \
-      KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  JP_UNDS, KC_PGUP, KC_PGDN, KC_END,  KC_ENT, \
+      KC_LCTL, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   JP_MINS, JP_EQL,  KC_PGUP, KC_PGDN, _______, \
+      KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  JP_UNDS, JP_BSLS, KC_HOME, KC_END , KC_ENT, \
       ADJUST,  KC_ESC,  KC_LGUI, KC_LALT, EISU,    _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
--- keyboards/helix/rev2/keymaps/shimarin/rules.mk
+++ keyboards/helix/rev2/keymaps/shimarin/rules.mk
@@ -18,7 +18,7 @@ RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.  Do not enable this
 ONEHAND_ENABLE = no        # Enable one-hand typing
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = yes    # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
