fa90fb7647cead5a88be97558a08fe97fb56075c
ikaruga <ikaruga777@gmail.com>
2018-03-03 23:40:43 +0900

    ビルド落ちてるやん

--- keyboards/helix/rev2/keymaps/shimarin/keymap.c
+++ keyboards/helix/rev2/keymaps/shimarin/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |  ;+  | :*   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |  ent | ent  |   N  |   M  |  ,<  |  .>  |  /?  |Shift |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |  B   | ent  |   N  |   M  |  ,<  |  .>  |  /?  |Shift |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * |Adjust| Esc  | GUI  | Alt  | EISU |Lower |Space |Space |Raise | KANA | Left | Down |  Up  |Right |
    * `-------------------------------------------------------------------------------------------------'
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH,  SFT_T(KC_ENT), \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_ENT,  KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, SFT_T(KC_ENT), \
       KC_LGUI, ADJUST,  KC_LGUI, KC_LALT, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
